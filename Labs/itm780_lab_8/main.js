249173f216ff90418cd24c9e7dd4e59f510d12d9
nvlasovtmu <nvlasov@torontomu.ca>
2024-11-08 17:41:29 -0500

    Lab 8

---
+++ Labs/itm780_lab_8/main.js
@@ -0,0 +1,48 @@
+
+function toggleColorblind(){
+    
+    var mainDivs = document.getElementsByTagName("div");
+
+    for (let i = 0; i < mainDivs.length; i++){
+
+        var currentBgColor = window.getComputedStyle(mainDivs[i]).backgroundColor;
+
+        if (currentBgColor === "rgb(27, 51, 97)") { // #1b3361 in rgb
+            mainDivs[i].style.backgroundColor = "#332288"; // Colorblind-friendly color
+
+            document.getElementById("buttonSmall").style.backgroundColor="#CC6677";
+            document.getElementById("buttonMed").style.backgroundColor="#CC6677";
+            document.getElementById("buttonLarge").style.backgroundColor="#CC6677";
+            document.getElementById("colorblindToggle").style.backgroundColor="#CC6677";
+
+            
+        } else {
+            mainDivs[i].style.backgroundColor = "#1b3361"; // Original color
+
+            document.getElementById("buttonSmall").style.backgroundColor="#a7d5fc";
+            document.getElementById("buttonMed").style.backgroundColor="#a7d5fc";
+            document.getElementById("buttonLarge").style.backgroundColor="#a7d5fc";
+            document.getElementById("colorblindToggle").style.backgroundColor="#a7d5fc";
+        }
+    }
+}
+
+document.getElementById("colorblindToggle").addEventListener("click", toggleColorblind);
+
+
+//Image Accessibility
+function displayAltText(altText){
+    alert("Image: " + altText);
+}
+
+document.getElementById("image1").addEventListener("click", function() {
+    displayAltText(document.getElementById("image1").alt);
+});
+
+document.getElementById("image2").addEventListener("click", function() {
+    displayAltText(document.getElementById("image2").alt);
+});
+
+document.getElementById("image3").addEventListener("click", function() {
+    displayAltText(document.getElementById("image3").alt);
+});
